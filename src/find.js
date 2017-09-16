9095077bef7941ccdc3ee75a38e89e8bbbabef11
OnLiU <jhs_onliu@naver.com>
2017-09-17 03:41:09 +0900

    rewrite logic: find

---
+++ src/find.js
@@ -0,0 +1,44 @@
+const request = require('request')
+const jsdom = require('jsdom')
+
+const getDomain = require('./getDomain')
+
+const initCallback = (err, result) => {
+  return (err)
+    ? console.error(err)
+    : console.log(result.map(({ name, code, type, address }) => [
+        `name: ${ name }`,
+        `code: ${ code }`,
+        `type: ${ type }`,
+        `address: ${ address }`
+      ].join('\n')).join('\n'))
+}
+
+module.exports = (__domain, query, callback = initCallback) => {
+  const domain = getDomain(__domain)
+  if (!domain) {
+    const err = `No such dep: ${ __domain }`
+
+    return callback(err, null)
+  }
+
+  const schoolName = encodeURIComponent(query)
+  const payload = {
+    uri: `http://${ domain }/spr_ccm_cm01_100.do?kraOrgNm=${ schoolName }`,
+    json: true
+  }
+
+  request(payload, (err, res, body) => {
+    if (err) return callback(err, null)
+
+    const foundData = body.resultSVO.orgDVOList
+    const result = foundData.map(({ kraOrgNm, orgCode, schulCrseScCodeNm, zipAdres }) => ({
+      name: kraOrgNm,
+      code: orgCode,
+      type: schulCrseScCodeNm,
+      address: zipAdres
+    }))
+
+    return callback(null, result)
+  })
+}
