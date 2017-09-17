1e7f1aa962e1c815460135ff3e6fb2c01bfde471
OnLiU <jhs_onliu@naver.com>
2017-09-17 18:34:45 +0900

    rewrite logic for readability
    change data-structure. forEach => filter. optimise return

--- src/getDomain.js
+++ src/getDomain.js
@@ -1,63 +1,38 @@
-const code = {
+const domains = {
   // A: ???
-  B: 'sen.go',
-  C: 'pen.go',
-  D: 'dge.go',
-  E: 'ice.go',
-  F: 'gen.go',
-  G: 'dje.go',
-  H: 'use.go',
-  I: 'sje.go',
-  J: 'goe.go',
-  K: 'gwe.go',
+  'sen.go':['B', 'SEN', 'SEOUL', '서울', '서울특별시'],
+  'pen.go': ['C', 'PEN', 'BUSAN', '부산', '부산광역시'],
+  'dge.go': ['D', 'DGE', 'DAEGU', '대구', '대구광역시'],
+  'ice.go': ['E', 'ICE', 'INCHEON', '인천', '인천광역시'],
+  'gen.go': ['F', 'GEN', 'GWANGJU', '광주', '광주광역시'],
+  'dje.go': ['G', 'DJE', 'DAEJEON', '대전', '대전광역시'],
+  'use.go': ['H', 'USE', 'ULSAN', '울산', '울산광역시'],
+  'sje.go': ['I', 'SJE', 'SEJONG', '세종', '세종시', '세종특별자치시'],
+  'goe.go': ['J', 'GOE', 'GYEONGGI', '경기', '경기도'],
+  'gwe.go': ['K', 'GWE', 'GANGWON', '강원', '강원도'],
   // L : ???
-  M: 'cbe.go',
-  N: 'cne.go',
+  'cbe.go': ['M', 'CBE', 'CHUNGBUK', '충북', '충청북도'],
+  'cne.go': ['N', 'CNE', 'CHUNGNAM', '충남', '충청남도'],
   // O : ???
-  P: 'jbe.go',
-  Q: 'jne.go',
-  R: 'gbe',
-  S: 'gne.go',
-  T: 'jje.go',
+  'jbe.go': ['P', 'JBE', 'JEONBUK', '전북', '전라북도'],
+  'jne.go': ['Q', 'JNE', 'JEONNAM', '전남', '전라남도'],
+  'gbe': ['R', 'GBE', 'GYEONGBUK', '경북', '경상북도'],
+  'gne.go': ['S', 'GNE', 'GYEONGNAM', '경남', '경상남도'],
+  'jje.go': ['T', 'JJE', 'JEUJU', '제주', '제주도']
 }
-
-const domains = [
-  // A: ???
-  ['B', 'sen', 'seoul', '서울', '서울특별시'],
-  ['C', 'pen', 'busan', '부산', '부산광역시'],
-  ['D', 'dge', 'daegu', '대구', '대구광역시'],
-  ['E', 'ice', 'incheon', '인천', '인천광역시'],
-  ['F', 'gen', 'gwangju', '광주', '광주광역시'],
-  ['G', 'dje', 'daejeon', '대전', '대전광역시'],
-  ['H', 'use', 'ulsan', '울산', '울산광역시'],
-  ['I', 'sje', 'sejong', '세종', '세종시', '세종특별자치시'],
-  ['J', 'goe', 'gyeonggi', '경기', '경기도'],
-  ['K', 'gwe', 'gangwon', '강원', '강원도'],
-  // L : ???
-  ['M', 'cbe', 'chungbuk', '충북', '충청북도'],
-  ['N', 'cne', 'chungnam', '충남', '충청남도'],
-  // O : ???
-  ['P', 'jbe', 'jeonbuk', '전북', '전라북도'],
-  ['Q', 'jne', 'jeonnam', '전남', '전라남도'],
-  ['R', 'gbe', 'gyeongbuk', '경북', '경상북도'],
-  ['S', 'gne', 'gyeongnam', '경남', '경상남도'],
-  ['T', 'jje', 'jeju', '제주', '제주도']
-]
 
 const prefix = 'stu'
 
 module.exports = (keyword) => {
-  let select = false
+  if (!keyword) return
 
-  domains.forEach((domain) => {
-    if (domain.includes(keyword)) {
-      select = domain[0]
-    }
-  })
+  const keywordUppered = keyword.toUpperCase()
 
-  const domain = code[select]
+  const code = Object.keys(domains)
+  const select = code
+    .filter((domain) => domains[domain].includes(keywordUppered))[0]
 
-  return (domain)
-    ? `${ prefix }.${ domain }.kr`
-    : false
+  if (select) {
+    return `${ prefix }.${ select }.kr`
+  }
 }
